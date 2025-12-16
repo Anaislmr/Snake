9e6d4d5c38705a7843885f86737a300a0b9229d0
Anais <anaislmr44@gmail.com>
2025-12-16 21:21:15 +0100

    commentaires

--- snake.cpp
+++ snake.cpp
@@ -2,6 +2,16 @@
 
 // g++ -Iextlib/pdcurses/include -Lextlib/pdcurses/lib snake.cpp -lpdcurses -o snake.exe
 
+// GitHub :
+// lien repository: https://github.com/Anaislmr/Snake.git
+
+// git init
+// git add
+// git commit -m "TEST"
+// git push
+
+//git rm test.txt
+
 
 #include <iostream>
 #include <fstream>
