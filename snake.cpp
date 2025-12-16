d3d90808f9d46335043ac86ec7d6fefd80aa27cf
Anais <anaislmr44@gmail.com>
2025-12-16 20:45:10 +0100

    First commit

---
+++ snake.cpp
@@ -0,0 +1,35 @@
+// A.MARIE --- PROJET SNAKE TRAINING --- 16.12.25
+
+// g++ -Iextlib/pdcurses/include -Lextlib/pdcurses/lib snake.cpp -lpdcurses -o snake.exe
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cmath>
+#include <cassert>
+#include "lib_projet_ncurses.hpp"
+
+using namespace std;
+
+//---------------------------------------------------------
+//                  ENREGISTREMENTS
+//---------------------------------------------------------
+
+//---------------------------------------------------------
+//                     SOUS-ALGOS
+//---------------------------------------------------------
+// void affichage_map
+
+//---------------------------------------------------------
+
+
+//---------------------------------------------------------
+//                        MAIN
+//---------------------------------------------------------
+int main() {
+    cout << "TEST";
+    return 0;
+}
